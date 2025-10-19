2ec4ea59b7ff3a68f627b80e9c100dfd8f7862e7
Ashwanth Fernando <ashwanthfernando@gmail.com>
2025-10-19 15:49:06 +1100

    Added topological sort

--- src/main/java/org/example/algorithms/graph/TopologicalSort.java
+++ src/main/java/org/example/algorithms/graph/TopologicalSort.java
@@ -1,4 +1,59 @@
 package org.example.algorithms.graph;
 
-public class TopologicalSort {
+import java.util.*;
+
+public class TopologicalSort<T> {
+
+    public static class Graph<T> {
+        private final Map<T, List<T>> map;
+
+        public Graph() {
+            this.map = new HashMap<>();
+        }
+
+        public void addEdge(T a, T b) {
+            map.computeIfAbsent(a, k -> new ArrayList<T>()).add(b);
+            map.computeIfAbsent(b, k -> new ArrayList<T>());
+        }
+
+        public List<T> getNeigbours(T a) {
+            return map.get(a);
+        }
+
+        public List<T> getVertices() {
+            return map.keySet().stream().toList();
+        }
+    }
+
+    public List<T> topoSort(Graph<T> graph) {
+        Set<T> visited = new HashSet<>();
+        Stack<T> stack = new Stack<>();
+
+        for (var vertex : graph.getVertices()) {
+            if (!visited.contains(vertex)) {
+                dfs(vertex, graph, stack, visited);
+            }
+        }
+
+        List<T> res = new ArrayList<>();
+
+        while (!stack.isEmpty()) {
+            res.add(stack.pop());
+        }
+
+        return res;
+    }
+
+    private void dfs(T vertex, Graph<T> graph, Stack<T> stack, Set<T> visited) {
+        visited.add(vertex);
+        if (graph.getNeigbours(vertex) == null) return;
+
+        for (T neighbour : graph.getNeigbours(vertex)) {
+            if (!visited.contains(neighbour)) {
+                dfs(neighbour, graph, stack, visited);
+            }
+        }
+
+        stack.push(vertex);
+    }
 }
---
+++ src/test/java/org/example/algorithms/graph/TopologicalSortTest.java
@@ -0,0 +1,25 @@
+package org.example.algorithms.graph;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class TopologicalSortTest {
+
+    @Test
+    public void test() {
+        var sorter = new TopologicalSort<Integer>();
+
+        TopologicalSort.Graph<Integer> graph = new TopologicalSort.Graph<>();
+
+        graph.addEdge(5, 2);
+        graph.addEdge(5, 0);
+        graph.addEdge(4, 0);
+        graph.addEdge(4, 1);
+        graph.addEdge(2, 3);
+        graph.addEdge(3, 1);
+
+        Assertions.assertEquals(sorter.topoSort(graph), List.of(5,4,2,3,1,0));
+    }
+}
